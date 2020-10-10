d93813f7bf24f2222d5ddd98bfb2629134594e13
0226daniel <0226daniel@gmail.com>
2020-10-11 00:57:00 +0900

    fix: error

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import logo from './logo.png';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="navigator">
-          <div>
-            RENDERING
-            <div className="underline"></div>
-          </div>
-          <div>ABOUT US</div>
-        </div>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<div className="navigator">
+					<div>
+						RENDERING
+						<div className="underline"></div>
+					</div>
+					<div>ABOUT US</div>
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- src/Rendering.js
+++ src/Rendering.js
@@ -1,37 +1,36 @@
 import './Rendering.css';
 import React, { useState, useEffect } from 'react';
-import SoundPool from './SoundPool';
+import SoundPool from './api/SoundPool';
 
-function rendering_page() {
-  const [audio, setAudio] = useState('');
+function Rendering() {
 	const [soundPool, setSoundPool] = useState(null);
 
 	useEffect(() => {
 		setSoundPool(new SoundPool());
-  }, []);
-  
-  return (
-    <div className="rendering_page">
-      <button
-					onClick={() => {
-						soundPool
-							.loadFromTTS('Hello, world! My name is tts that in rakuten api.')
-							.then((id) => {
-								console.log(id);
-							});
-					}}
-				>
-					prepareTTS
-				</button>
-				<button
-					onClick={() => {
-						soundPool.play(0);
-					}}
-				>
-					playTTS
-				</button>
-    </div>
-  );
+	}, []);
+
+	return (
+		<div className="rendering_page">
+			<button
+				onClick={() => {
+					soundPool
+						.loadFromTTS('Hello, world! My name is tts that in rakuten api.')
+						.then((id) => {
+							console.log(id);
+						});
+				}}
+			>
+				prepareTTS
+			</button>
+			<button
+				onClick={() => {
+					soundPool.play(0);
+				}}
+			>
+				playTTS
+			</button>
+		</div>
+	);
 }
 
-export default rendering_page;
+export default Rendering;
