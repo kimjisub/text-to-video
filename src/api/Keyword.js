92c33902ff342015a758b245fe12e0b652ec91ec
0226daniel <0226daniel@gmail.com>
2020-10-11 11:31:13 +0900

    fix: update keyword api

--- src/api/Keyword.js
+++ src/api/Keyword.js
@@ -1,51 +1,30 @@
 /* eslint-disable no-unreachable */
 import unirest from 'unirest';
-
-function shuffleArray(array) {
-	for (var i = array.length - 1; i > 0; i--) {
-		var j = Math.floor(Math.random() * (i + 1));
-		var temp = array[i];
-		array[i] = array[j];
-		array[j] = temp;
-	}
-	return array;
-}
 export default class Keyword {
 	async getKeywords(text) {
 		return new Promise((resolve, reject) => {
-			return resolve(
-				shuffleArray([
-					'love',
-					'home',
-					'cat',
-					'dog',
-					'game',
-					'age',
-					'sky',
-					'smile',
-				])
-			);
-			const req = unirest(
+			var req = unirest(
 				'POST',
-				'https://textanalysis-keyword-extraction-v1.p.rapidapi.com/keyword-extractor-text'
+				'https://webit-text-analytics.p.rapidapi.com/key-phrases'
 			);
 
+			req.query({
+				language: 'en',
+				q: text,
+			});
+
 			req.headers({
-				'x-rapidapi-host': 'textanalysis-keyword-extraction-v1.p.rapidapi.com',
+				'x-rapidapi-host': 'webit-text-analytics.p.rapidapi.com',
 				'x-rapidapi-key': '4138961e01msh4f4638e80f2ad91p1cd360jsnc91a35ded75e',
 				'content-type': 'application/x-www-form-urlencoded',
 				useQueryString: true,
 			});
 
-			req.form({
-				text,
-				wordnum: '5',
-			});
-
+			req.form({});
 			req.end(function (res) {
 				if (res.error) throw new Error(res.error);
 
-				resolve(res.body.keywords);
+				resolve(res.body.data.key_phrases);
 			});
 		});
 	}
