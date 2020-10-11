f5ca17d2957ad75bb43e57af21a7a3b5a8c18c33
0226daniel <0226daniel@gmail.com>
2020-10-11 16:35:50 +0900

    feat: auto make items

--- src/Rendering.js
+++ src/Rendering.js
@@ -20,7 +20,7 @@ import {
 	InputLabel,
 	FormControl,
 } from '@material-ui/core';
-import { VolumeUp, PlayArrow, Pause } from '@material-ui/icons';
+import { PlayArrow, Pause } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
@@ -49,10 +49,12 @@ const useStyles = makeStyles({
 });
 
 const scripts = {
-	'covid-19':
+	'Covid-19':
 		'As of the Chuseok and Hangeul Day holidays, the confirmed coronavirus 19 (COVID-19) remains in double digits. So there is a growing likelihood that the government will ease the distancing steps applied from the 12th.',
-	flower:
+	Flower:
 		'You can cut all the flowers. but you can not keep spring from coming.',
+	Phrase:
+		"There is no place like home. Love will find a way. Slow and steady win the game. Life's not all gloom and despondency. Age does not protect you from love. Believe you can, then you will. If I have lost confidence in myself, I have the universe against me. Hold it high, look the world straight in the eye. Better the last smile than the first laughter. Behind the cloud is the sun still shining. ",
 };
 
 let timeout = -1;
@@ -106,8 +108,11 @@ function Rendering() {
 								setScriptText(scripts[event.target.value]);
 							}}
 						>
-							<MenuItem value="covid-19">Covid-19</MenuItem>
-							<MenuItem value="flower">Flower</MenuItem>
+							{Object.keys(scripts).map((k) => (
+								<MenuItem value={k} key={k}>
+									{k}
+								</MenuItem>
+							))}
 						</Select>
 					</FormControl>
 					<TextField
